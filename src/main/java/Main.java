f1fad1c9e898b6ad23b927440f70c3762b00ff01
Victor <FilimonovVA.95@mail.ru>
2020-06-24 17:49:20 +0300

    Task complected

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,42 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+        String folderDrivers = "drivers\\chromedriver.exe";
+        int timeWait = 5;
 
+        String testURL = "https://uxcrowd.ru/";
+        String loginName = "";
+        String loginPassword = "";
+
+        System.setProperty("webdriver.chrome.driver", folderDrivers);
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://uxcrowd.ru/");
+        driver.get(testURL);
+
+        if (driver.findElement((By.xpath("//a[@id='header-lk-button']"))).isDisplayed())
+            driver.findElement((By.xpath("//a[@id='header-lk-button']"))).click();
+        else
+            driver.findElement((By.xpath("//img[@id='mobileAvatar']"))).click();
+
+        new WebDriverWait(driver, timeWait).until(ExpectedConditions
+                .presenceOfElementLocated(By.xpath("//div[@class='modal-body newprice-modal ng-scope']")));
+
+        driver.findElement(By.xpath("//input[@id='login']")).sendKeys(loginName);
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(loginPassword);
+        driver.findElement((By.xpath("//form[@id='form_auth']//button[@class='lk-enter-btn']"))).click();
+
+        new WebDriverWait(driver, timeWait).withMessage("Log in exception")
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@id='logout']")));
+
+        driver.findElement((By.xpath("//a[@id='logout']"))).click();
+
+        driver.quit();
     }
 }
